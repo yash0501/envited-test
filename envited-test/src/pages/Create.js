e9bd14a8d07a411abc802c2bf19fbfe1e476eaac
yash0501 <yashgarg5101@gmail.com>
2022-10-05 03:12:54 +0530

    Updated Create Page

--- envited-test/src/pages/Create.js
+++ envited-test/src/pages/Create.js
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Routes, Route, useNavigate} from "react-router-dom"
-import "../Landing.css"
+import "../Create.css"
 
 function Create() {
+    const [image, setImage] = useState(null);
     const navigate = useNavigate()
 
     const navigateToEvent = () => {
         navigate("/event")
     }
+
+    const onImageUpload = (event)=>{
+      if(event.target.files && event.target.files[0]){
+        setImage(URL.createObjectURL(event.target.files[0]));
+      }
+    }
+
   return (
     <div className='create-page'>
-      <h1>This is Create</h1>
-
-        <div className='add-event-btn'>
-            <button className='add-event-btn-text' onClick={navigateToEvent}>Next</button>
-        </div>
-
+      <h2 className="create-heading">Create Event</h2>
+      <h4 className="create-subheading">Invite your friends to binge watch with you.</h4>
+      <form className="form">
+        <input type="text" placeholder="Event Name" className="text-field" />
+        <input type="text" placeholder="Host Name" className="text-field" />
+        <input type="datetime-local" placeholder="Start Date/Time" className="text-field" />
+        <input type="datetime-local" placeholder="End Date/Time" className="text-field" />
+        <input type="text" placeholder="Host Name" className="text-field" />
+        <input type="file" placeholder="Upload Image" className="text-field" onChange={onImageUpload} />
+        {image && <img src={image} className="display-img" />}
+        
+      </form>
+      <div className='add-create-btn'>
+          <button className='add-create-btn-text' onClick={navigateToEvent}>Next</button>
+      </div>
     </div>
   )
 }
